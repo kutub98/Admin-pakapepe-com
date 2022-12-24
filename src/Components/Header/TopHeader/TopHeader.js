71aeaeb20d5307017c1c9c2c19d0fa04a81a6ca4
kutub98 <kutubuddin98710@gmail.com>
2022-12-25 02:45:31 +0600

    added

--- src/Components/Header/TopHeader/TopHeader.js
+++ src/Components/Header/TopHeader/TopHeader.js
@@ -5,19 +5,19 @@ import { FaShoppingCart } from "react-icons/fa";
 const TopHeader = () => {
   const topHeader = [
   <React.Fragment>
-    <ul className="flex justify-between items-center">
-        <div className="logo">
-        <li><Link to='home'> Welcome to PakaPepe </Link></li>
+    <ul className="flex justify-between items-center text-center">
+        <div className="logo text-center">
+        <li className="text-center"><Link to='home'> Welcome to PakaPepe </Link></li>
         </div>
-        <div className="logo flex items-center justify-between">
+        {/* <div className="logo flex items-center justify-between">
         <li className="border border-l text-xl font-semi-bold"><Link to='LoginBox'>Login</Link></li>
         <li className="border border-l ml-4"><p> <FaShoppingCart className="w-6 h-6 text-red-600"/> </p></li>
-        </div>
+        </div> */}
     </ul>
   </React.Fragment>
   ];
 
-  return <div className="px-[4%] py-2 bg-slate-100 border bottom-2 ">
+  return <div className="px-[4%] py-2 bg-slate-100 border bottom-2 text-center">
     {topHeader}
   </div>;
 };
